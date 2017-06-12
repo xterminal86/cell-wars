b29a58c9155a89eb4d59c2d32d784ba4af7a41df
xterminal86 <xterminal86@gmail.com>
2017-06-13 01:03:52 +0300

    pathfinding test

---
+++ Assets/scripts/for-testing/PathfindingCell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingCell : MonoBehaviour 
+{
+  public TextMesh TextMapOverlay;
+  public TextMesh TextPathOverlay;
+
+  public void SetMapOverlay(string text)
+  {
+    TextMapOverlay.text = text;
+  }
+
+  public void SetPathOverlay(string text)
+  {
+    TextPathOverlay.text = text;
+  }
+
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.Tab))
+    {
+      TextMapOverlay.gameObject.SetActive(false);
+      TextPathOverlay.gameObject.SetActive(true);
+    }
+    else
+    {
+      TextMapOverlay.gameObject.SetActive(true);
+      TextPathOverlay.gameObject.SetActive(false);
+    }
+  }
+}
